6cd285cd614eb858b0fae20cfbb16e935fabef53
Zintis ICS3U <zintis.kalins@student.tdsb.on.ca>
2024-12-10 09:51:13 -0500

    created pieces.py file

---
+++ pieces.py
@@ -0,0 +1,17 @@
+import pygame
+
+class pieces:
+
+    def king(colour):
+    
+    def queen(colour):
+    
+    def bishop(colour):
+    
+    def knight(colour):
+    
+    def rook(colour):
+    
+    def pawn(colour):
+    
+    
